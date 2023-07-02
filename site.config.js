cc0674b6c017f1ad3d3fe67f296565f88b271abd
CCPartners <137975272+CCPartners@users.noreply.github.com>
2023-07-03 00:39:50 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "triptoday",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "master@ccpartners.co.kr",
+    linkedin: "triptoday",
+    github: "CCPartners",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `triptoday-blog`,
+      href: "https://github.com/CCPartners/triptoday-blog",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "triptoday-blog",
+    description: "welcome to triptoday blog!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
@@ -51,9 +51,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "CCPartners/triptoday-blog",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
